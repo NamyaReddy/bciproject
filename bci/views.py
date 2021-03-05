a38b9ee1c65dcef9a3d819e825f20b8c0fabe510
NamyaReddy <gnamyareddy25@gmail.com>
2021-03-05 20:32:26 +0530

    Login

---
+++ bci/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from django.http import HttpResponse,JsonResponse
+import requests
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_control,never_cache
+from django.utils.cache import add_never_cache_headers
+
+
+def login(request):
+    context={'data':''}
+    return render(request,'login.html',context)
+
--- bciproject/settings.py
+++ bciproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bci.apps.BciConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
